da1a1ef9d28b9675f387bff35d778f65ff3453cb
whitej68 <114672020+whitej68@users.noreply.github.com>
2023-10-20 17:49:13 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QMainWindow
+
+
+class ChatbotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(700, 500)
+
+        # Add chat area widget
+
+        # Add input field widget
+
+
+class Chatbot:
+    pass
